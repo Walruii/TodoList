df569b3a5a83d47be4fe6222dc90fada38403633
walruii <walruiix@gmail.com>
2023-08-12 15:25:24 +0530

    fixed footer and some css

--- todoList/app.js
+++ todoList/app.js
@@ -10,15 +10,15 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 const itemsSchema = {
-    name: String
+  name: String
 };
 
 const Item = mongoose.model("Item", itemsSchema);
 const date = require(__dirname + "/date.js");
 
 const listSchema = {
-    name: String,
-    items: [itemsSchema]
+  name: String,
+  items: [itemsSchema]
 };
 
 const List = mongoose.model("List", listSchema);
@@ -27,116 +27,114 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
 const item1 = new Item({
-    name: "GAMING"
+  name: "GAMING"
 });
 
 const item2 = new Item({
-    name: "MORE GAMING"
+  name: "MORE GAMING"
 });
 
 const defaultItems = ([item1, item2]);
-
-// Item.insertMany([item1, item2]);
 
 let items = [];
 
 
 app.get("/", async (req, res) => {
-    items = [];
+  items = [];
 
-    const itm = await Item.find({});
-    const itmLeg = await itm.length;
+  const itm = await Item.find({});
+  const itmLeg = await itm.length;
 
-    if (itmLeg === 0) {
+  if (itmLeg === 0) {
 
-        Item.insertMany([item1, item2]);
-        res.redirect("/");
+    Item.insertMany([item1, item2]);
+    res.redirect("/");
+    return;
+  }
+  itm.forEach(function(item) {
 
-    }
-    itm.forEach(function(item) {
+    items.push(item);
 
-        items.push(item);
+  });
 
-    });
+  res.render("list", {
 
-    res.render("list", {
+    listName: "Today",
+    items: items,
 
-        listName: "Today",
-        items: items,
-
-    });
+  });
 });
 
 app.get("/:list", async (req, res) => {
 
-    const listName = _.capitalize(req.params.list);
+  const listName = _.capitalize(req.params.list);
 
-    List.findOne({ name: listName }).then(function(foundList) {
+  List.findOne({ name: listName }).then(function(foundList) {
 
-        if (!foundList) {
+    if (!foundList) {
 
-            const list = new List({
-                name: listName,
-                items: defaultItems
-            });
+      const list = new List({
+        name: listName,
+        items: defaultItems
+      });
 
-            list.save();
+      list.save();
 
-            res.redirect("/" + listName);
+      res.redirect("/" + listName);
 
-        } else {
+    } else {
 
-            res.render("list", {
-                listName: foundList.name,
-                items: foundList.items
-            });
-        }
-    });
+      res.render("list", {
+        listName: foundList.name,
+        items: foundList.items
+      });
+    }
+  });
 });
 
 app.post("/", async (req, res) => {
 
-    const reqItem = req.body.item;
-    const listName = req.body.list;
+  const reqItem = req.body.item;
+  const listName = req.body.list;
 
-    const item = new Item({
-        name: reqItem,
+  const item = new Item({
+    name: reqItem,
+  });
+
+  if (listName === "Today") {
+
+    const response = await Item.insertMany([item]);
+
+    res.redirect("/");
+
+  } else {
+
+    List.findOne({ name: listName }).then(function(foundList) {
+      foundList.items.push(item);
+      foundList.save();
     });
 
-    if (listName === "Today") {
-
-        const response = await Item.insertMany([item]);
-
-        res.redirect("/");
-
-    } else {
-
-        List.findOne({ name: listName }).then(function(foundList) {
-            foundList.items.push(item);
-            foundList.save();
-        });
-
-        res.redirect("/" + listName);
-    }
+    res.redirect("/" + listName);
+  }
 });
 
 app.post("/delete", async (req, res) => {
 
-    const id = req.body.checkbox;
-    const listName = req.body.list;
+  const id = req.body.checkbox;
+  const listName = req.body.list;
 
 
-    if (listName === "Today") {
+  if (listName === "Today") {
 
-        const rm = await Item.findOneAndDelete({ _id: id });
-        res.redirect("/");
+    const rm = await Item.findOneAndDelete({ _id: id });
+    res.redirect("/");
 
-    } else {
+  } else {
 
-        const rm = await List.findOneAndUpdate({name: listName}, { $pull: {items: {_id: id}}});
-        res.redirect("/" + listName);
-    }
+    const rm = await List.findOneAndUpdate({ name: listName }, { $pull: { items: { _id: id } } });
+    res.redirect("/" + listName);
+  }
 });
 
 
-app.listen(process.env.PORT || 3000, function () {});
+app.listen(process.env.PORT || 3000, function() { });
